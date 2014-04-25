0bfd47f21c25c63c7d2d2158899cc689f3eee3d8
danilodanicomendes <ddannillo@gmail.com>
2014-04-25 20:19:04 +0100

    Estruturação, falta só mais 99%

--- src/gameLogic/states/StateAdapter.java
+++ src/gameLogic/states/StateAdapter.java
@@ -1,5 +1,61 @@
 package gameLogic.states;
 
 public abstract class StateAdapter implements StateInterface {
+    @Override
+    public StateInterface defineGame() {
+        return this;
+    }
+    @Override
+    public StateInterface defineWinner() {
+        return this;
+    }
+    @Override
+    public StateInterface defineCard() {
+        return this;
+    }
+    @Override
+    public StateInterface defineArmy() {
+        return this;
+    }
+    @Override
+    public StateInterface defineMoveByLand() {
+        return this;
+    }
+    @Override
+    public StateInterface defineMoveBySea() {
+        return this;
+    }
+    @Override
+    public StateInterface defineCity() {
+        return this;
+    }
+    @Override
+    public StateInterface defineNeutralize() {
+        return this;
+    }
+    
+    @Override
+    public StateInterface placeArmyOrCity() {
+        return this;
+    }
+    @Override
+    public StateInterface moveArmy() {
+        return this;
+    }
+    @Override
+    public StateInterface neutralize() {
+        return this;
+    }
+    
+    @Override
+    public StateInterface saveToFile() {
+        return this;
+    }
+
+    @Override
+    public StateInterface loadFromFile() {
+        return this;
+    }
+    
     
 }
--- src/gameLogic/states/StateInterface.java
+++ src/gameLogic/states/StateInterface.java
@@ -1,5 +1,20 @@
 package gameLogic.states;
 
+// Os argumentos podem não ser os mais correctos
 public interface StateInterface {
+    public StateInterface defineGame();
+    public StateInterface defineWinner();
+    public StateInterface defineCard();
+    public StateInterface defineArmy();
+    public StateInterface defineMoveByLand();
+    public StateInterface defineMoveBySea();
+    public StateInterface defineCity();
+    public StateInterface defineNeutralize();
     
+    public StateInterface placeArmyOrCity();
+    public StateInterface moveArmy();
+    public StateInterface neutralize();
+    
+    public StateInterface saveToFile();
+    public StateInterface loadFromFile();
 }
